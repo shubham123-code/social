def8dff656faac7396b6d1fecf0524c0a0f24292
shubham <maple@maples-MacBook-Pro.local>
2022-10-11 15:58:47 +0530

    i am done

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from core.models import Profile
+from core.models import Profile, Post
 
 
 # Create your views here.
@@ -18,10 +18,20 @@ def index(request):
     print(type(request))
     user_object = User.objects.get(username=request.user.username)
     user_profile = Profile.objects.get(user=user_object)
-    return render(request, 'index.html', {'user_profile': user_profile})
+    
+    return render(request, 'index.html', {'user_profile':user_profile})
 @login_required(login_url='signin')
 def upload(request):
-    return HttpResponse('<h1>Upload View</h1>')
+    if request.method=='POST':
+        user = request.user.username
+        image = request.FILES.get('image_upload')
+        caption = request.POST['caption']
+        new_post = Post.objects.create(user=user, image=image, caption=caption)
+        new_post.save()
+        return redirect('/')
+    else:
+        return redirect('/')
+    
 @login_required(login_url='signin')
 def settings(request):
     user_profile = Profile.objects.get(user=request.user)
@@ -34,6 +44,7 @@ def settings(request):
             user_profile.bio = bio
             user_profile.location = location
             user_profile.save()
+ 
         else:
             image = request.FILES.get('image')
             bio = request.POST['bio']
